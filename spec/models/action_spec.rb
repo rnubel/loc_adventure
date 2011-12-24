0b97c987607967a546057e5824547bebf401868a
Robert Nubel <robert.nubel@gmail.com>
2011-12-24 00:42:24 -0600

    Updates action tests to be compliant.

--- spec/models/action_spec.rb
+++ spec/models/action_spec.rb
@@ -46,20 +46,11 @@ describe Action do
     action.effective_on.should == Date.today + 10.days
   end
 
-  it "should be effectable" do
-    action = Factory.create(:action,
-               :type => Action::Type::DRAW,
-               :status => Action::Status::SCHEDULED,
-               :effective_on => Date.today)
-
-    lambda { action.effect! }.should_not raise_error
-  end
-
   it "should change its status to effected when effected" do
-    action = Factory.create(:action,
-               :type => Action::Type::DRAW,
-               :status => Action::Status::SCHEDULED,
-               :effective_on => Date.today)
+    action = Action.new
+    action.expects(:effectable?).returns(true)
+    action.expects(:type).returns(stub(:transactions => []))
+
     action.effect!
 
     action.status.should == Action::Status::EFFECTED
@@ -77,16 +68,16 @@ describe Action do
                  :effective_on => Date.today,
                  :amount => 100.0)
       account1 = Factory.create(:account, :account_type => Account::Type::CUSTOMER)
-      account2 = Factory.create(:account, :account_type => Account::Type::ACCOUNTS_RECEIVABLE)
+      account2 = Factory.create(:account, :account_type => Account::Type::PRINCIPAL)
       @action.line_of_credit.expects('find_account').with(:customer).returns(account1)
-      @action.line_of_credit.expects('find_account').with(:accounts_receivable).returns(account2)
+      @action.line_of_credit.expects('find_account').with(:principal).returns(account2)
       @action.effect!
     end
 
     it "should create a transaction from customer to receivable when effected" do
       @action.transactions.all? { |tr|
         tr.debit_account.account_type.should == Account::Type::CUSTOMER
-        tr.credit_account.account_type.should == Account::Type::ACCOUNTS_RECEIVABLE
+        tr.credit_account.account_type.should == Account::Type::PRINCIPAL
         tr.amount.should == 100.0
       }.should == true
     end
